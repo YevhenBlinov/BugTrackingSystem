6552aed2194acc2e68f2292cb4eb585d8f2fb123
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-07-03 13:38:14 +0300

    Added user-, bug-, project view models

---
+++ BugTrackingSystem.Web/Models/ProjectViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BugTrackingSystem.Web.Models
+{
+    public class ProjectViewModel
+    {
+        public int ProjectID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Prefix { get; set; }
+
+        public ICollection<BugViewModel> Bugs { get; set; }
+
+        public ICollection<UserViewModel> Users { get; set; }
+    }
+}
---
+++ BugTrackingSystem.Web/Models/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BugTrackingSystem.Web.Models
+{
+    public class UserViewModel
+    {
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Photo { get; set; }
+
+        public ICollection<BugViewModel> Bugs { get; set; }
+
+        public ICollection<ProjectViewModel> Projects { get; set; }
+    }
+}
