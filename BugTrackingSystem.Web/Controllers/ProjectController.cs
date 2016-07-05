80f44f014133357b9b17c4884c44793e820026b5
chuiguk <chuiguk.eu@gmail.com>
2016-07-06 00:28:44 +0300

    Edited views and controllers

--- BugTrackingSystem.Web/Controllers/ProjectController.cs
+++ BugTrackingSystem.Web/Controllers/ProjectController.cs
@@ -1,14 +1,40 @@
 ﻿using System.Web.Mvc;
+using BugTrackingSystem.Service.Services;
 
 namespace BugTracking.Controllers
 {
     public class ProjectController : Controller
     {
+        private IProjectService _projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
         //
         // GET: /Project/
-        public ActionResult Project()
+        [HttpGet]
+        public ActionResult Project(int projectId)
         {
+            var projects = _projectService.GetProjectById(projectId);
+            return View(projects);
+        }
+
+        public ActionResult Projects()
+        {
+
             return View();
+        }
+
+        public ActionResult ProjectUsers()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectBugs()
+        {
+            return PartialView();
         }
     }
 }
