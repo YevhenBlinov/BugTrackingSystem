6ad91103bea30a70bcee48e34c596f83d8649e45
chuiguk <chuiguk.eu@gmail.com>
2016-07-06 15:39:44 +0300

    Edited views and controllers

--- BugTrackingSystem.Web/Controllers/ProjectController.cs
+++ BugTrackingSystem.Web/Controllers/ProjectController.cs
@@ -6,24 +6,26 @@ namespace BugTrackingSystem.Web.Controllers
     public class ProjectController : Controller
     {
         private IProjectService _projectService;
+        private IUserService _userService;
 
-        public ProjectController(IProjectService projectService)
+        public ProjectController(IProjectService projectService, IUserService userService)
         {
             _projectService = projectService;
+            _userService = userService;
         }
         //
         // GET: /Project/
         [HttpGet]
         public ActionResult Project(int projectId)
         {
-            var projects = _projectService.GetProjectById(projectId);
-            return View(projects);
+            var project = _projectService.GetProjectById(projectId);
+            return View(project);
         }
 
-        public ActionResult Projects()
+        public ActionResult Projects(int userId = 1)
         {
-
-            return View();
+            var projects = _userService.GetUsersProjects(userId);
+            return View(projects);
         }
 
         public ActionResult ProjectUsers()
