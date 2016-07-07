400563e3a707397a5723769cb3419148ac03b302
chuiguk <chuiguk.eu@gmail.com>
2016-07-08 00:04:24 +0300

    Edited views & added Task view

--- BugTrackingSystem.Web/Controllers/TaskController.cs
+++ BugTrackingSystem.Web/Controllers/TaskController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Web.Mvc;
+using BugTrackingSystem.Service.Services;
 
 namespace BugTrackingSystem.Web.Controllers
 {
     public class TaskController : Controller
     {
+        private readonly IBugService _bugService;
+
+        public TaskController(IBugService bugService)
+        {
+            _bugService = bugService;
+        }
         //
         // GET: /Task/
-        public ActionResult Task()
+        public ActionResult Task(int bugId = 1)
         {
-            return View();
+            var info = _bugService.GetFullBugById(bugId);
+            return View(info);
         }
     }
 }
