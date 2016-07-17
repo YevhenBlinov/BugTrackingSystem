cb2e9fd8833bb0d1c9a45d59baf57c9e83b43a40
Yevhen Blinov <yevhen.blinov@gmail.com>
2016-07-17 17:12:17 +0300

    Added AdvancedFilterViewModel

---
+++ BugTrackingSystem.Service/Models/AdvancedFilterViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BugTrackingSystem.Service.Models
+{
+    public class AdvancedFilterViewModel
+    {
+        public int FilterId { get; set; }
+
+        public string Title { get; set; }
+
+        public int[] Project { get; set; }
+
+        public int[] AssignedUser { get; set; }
+
+        public string Search { get; set; }
+
+        public string[] BugStatus { get; set; }
+
+        public string[] BugPriority { get; set; }
+    }
+}
